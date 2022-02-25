6e13c11f55ad2b24e911f576ef1cc31d91f764f1
Cecile <cecile.honnorat@gmail.com>
2022-02-25 19:12:01 +0100

    routes

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,8 @@ router.post('/signup', function(req, res, next) {
   }
 });
 
+
+// Connection
 router.post('/signin', function(req, res, next) {
   var email = req.body.email
   var password = req.body.password
@@ -36,6 +38,7 @@ router.post('/signin', function(req, res, next) {
   }
 })
 
+// Récuperation des profils users
 router.get('/users', function(req, res, next) {
   var id = req.query.id;
 
@@ -47,6 +50,71 @@ router.get('/users', function(req, res, next) {
       description: 'coucou !'
     }]})
   }
+})
+
+// Récupération des positions des users
+router.get('/users-position', function(req, res, next){
+  res.json()
+})
+
+// Récupération des notes 
+router.get('/rate', function(req, res, next){
+  res.json()
+})
+
+// Ajout d'un favoris
+router.post('/add-favorite', function(req, res, next){
+  res.json()
+})
+
+// Suppression d'un favoris
+router.delete('/delete-favorite/', function(req, res, next){
+  res.json()
+})
+
+// Récupération des favoris
+router.get('/favorites', function(req, res, next){
+  res.json()
+})
+
+// Récupération des conversations
+router.get('/conversations', function(req, res, next){
+  res.json()
+})
+
+// Suppression d'une conversation 
+router.delete('/delete-conversation', function(req, res, next){
+  res.json()
+})
+
+// Récupération d'un chat pour afficher les messages entre 2 users
+router.get('/chat', function(req, res, next){
+  res.json()
+})
+
+// Envoi d'un message
+router.post('/send-message', function(req, res, next){
+  res.json()
+})
+
+// Récupération de l'agenda
+router.get('/agenda', function(req, res, next){
+  res.json()
+})
+
+// Ajouter un rendez-vous
+router.post('/add-date', function(req, res, next){
+  res.json()
+})
+
+// Modifier info d'un user 
+router.put('/settings', function(req, res, next){
+  res.json()
+})
+
+// Ecrire une review
+router.post('/add-review', function(req, res, next){
+  res.json()
 })
 
 
