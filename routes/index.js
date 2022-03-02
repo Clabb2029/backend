f62ee994924003e5c135f079ec8338a5134c0619
mersile-atti <oumarmersileattimahamat@yahoo.fr>
2022-03-02 16:26:08 +0100

    Routes en backend finalisées

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+const dotenv = require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const userModel = require('../models/users');
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
