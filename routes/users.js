bba7fcf3e5a2ca1d64f34e6a1b2ccf2e83e8f299
Cecile <cecile.honnorat@gmail.com>
2022-03-05 16:09:06 +0100

    signup et roken redux

--- routes/users.js
+++ routes/users.js
@@ -9,115 +9,108 @@ const userModel = require('../models/users');
 
 /* GET users listing. avant le prefixe users et ça marchait très bien */
 router.post('/signup', async (req, res) => {
-const { 
-          pseudo,
-          email, 
-          password,
-          optinEmails,
-          status,
-        } = req.body
+  var error = '';
+  var result = false;
 
-        console.log(req.body)
-  if(!pseudo || !email || !password) {
-      res.status(400)
-      throw new Error('Veuillez remplir tous les champs!')
+  if (req.body.pseudo == '' || req.body.email == '' || req.body.password == '') {
+    error = 'Veuillez remplir tous les champs!'
+    result = false
   }
 
-  const userExist = await userModel.findOne({email})
-  if(userExist) {
-      res.status(400);
-      throw new Error('Utisateur déjà existant!')
+  var userExist = await userModel.findOne({ email : req.body.email })
+  if (userExist != null) {
+    error = "email déjà utilisé";
+    result = false
   }
   //hash password
-  var hash = bcrypt.hashSync(password, 10);
+  var hash = bcrypt.hashSync(req.body.password, 10);
 
   //And then create the user: 
-  var newUser = await userModel({
-    pseudo : pseudo,
-    email : email,
-    optinEmails : optinEmails,
-    password : hash,
-    status : status,
-    token: uid2(32),
-  })
+  if (userExist == null && req.body.pseudo!= '' && req.body.email != '' && req.body.password!= '') {
+    var newUser = await userModel({
+      pseudo: req.body.pseudo,
+      email: req.body.email,
+      optinEmails: req.body.optinEmails,
+      password: hash,
+      status: req.body.status,
+      token: uid2(32),
+    })
 
-  var userSaved = await newUser.save();
-  if(userSaved) {
-
-      res.status(201).json(
-          {
-            token : userSaved.token,
-            result : true
-          }
-      )
-  
-  } else {
-          res.status(404)
-          throw new Error('Les informations sont invalides!')
-      }
- 
+    var userSaved = await newUser.save();
+    result = true
+  }
+  console.log(result)
+  console.log(error)
+  res.json(
+    {
+      error,
+      result,
+      userSaved
+    }
+  )
 });
 
 // Route sign-up more avec plus d'infos sur le user:
 
-router.put('/signup-more/:token', async(req, res) => {
+router.put('/signup-more/:token', async (req, res) => {
   console.log(req.body)
-  const { 
+  const {
     zipcode,
     city,
-      livingPlace, 
-      petChoice,
+    livingPlace,
+    guardType,
+  } = req.body
+
+
+  const userExist = await userModel.find({ token: req.params.token });
+  //check if user exist
+  if (!userExist) {
+    res.status(401)
+    throw new Error('User not found')
+  }
+
+  const updateUser = await userModel.updateOne({ token: req.params.token },
+    {
+      livingPlace,
       guardType,
-    } = req.body
-
-
-  const userExist = await userModel.find({token: req.params.token});
-    //check if user exist
-    if(!userExist) {
-        res.status(401)
-        throw new Error('User not found')
-    }
-
-    const updateUser = await userModel.updateOne({token: req.params.token},
-      {livingPlace, 
-        petChoice,
-        guardType,
-        $push: { address: {
+      $push: {
+        address: {
           zipcode,
           city
-        }}
+        }
       }
-      )
-      console.log(updateUser)
-      
-    res.status(200).json({result: true}) 
+    }
+  )
+  console.log(updateUser)
+
+  res.status(200).json({ result: true })
 })
 
 // Route Signin user :
 router.post('/users/signin', async (req, res) => {
-  const {email, password} = req.body;
+  const { email, password } = req.body;
 
   //check for the user email
 
-  const user = await userModel.findOne({email})
+  const user = await userModel.findOne({ email })
 
-  if(user && (await bcrypt.compare(password, user.password))) {
-      res.json({
-          _id: user.id,
-          email: user.email,
-          token: generateToken(user._id)
-      })
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      email: user.email,
+      token: generateToken(user._id)
+    })
   } else {
-      res.status(404)
-      throw new Error('Informations invalides')
+    res.status(404)
+    throw new Error('Informations invalides')
   }
-  
-  res.json({message: 'Login User'})
+
+  res.json({ message: 'Login User' })
 })
 
 const generateToken = (id) => {
   return jwt.sign({ id }, `${process.env.JWT_SECRET}`, {
-      expiresIn: "30d",
+    expiresIn: "30d",
   })
 }
 module.exports = router;
