b157859766209235ad53974b79cf6dbfc483100e
Cecile <cecile.honnorat@gmail.com>
2022-03-08 12:36:59 +0100

    upload images

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var request = require('sync-request')
 const dotenv = require('dotenv').config();
 
+//Pour générer un token :
 var uid2 = require('uid2')
+// Pour crypter le mot de passe
 var bcrypt = require('bcrypt');
+
+// Pour l'upload et le stockage des images
+const uniqid = require('uniqid');
+const fs = require('fs');
+
+
+var cloudinary = require('cloudinary').v2;
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
+});
 
 const userModel = require('../models/users');
 
@@ -23,13 +38,14 @@ router.post('/signup', async (req, res) => {
     result = false
   }
 
-var emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-var email = req.body.email
-if (emailregex.test(req.body.email) == false )
-{ result = false
-  error = "Veuillez saisir un email valide ! "}
+  var emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  var email = req.body.email
+  if (emailregex.test(req.body.email) == false) {
+    result = false
+    error = "Veuillez saisir un email valide ! "
+  }
   //And then create the user: 
-  if (userExist == null && req.body.pseudo != '' && req.body.email != '' && req.body.password != '' && emailregex.test(req.body.email) == true ) {
+  if (userExist == null && req.body.pseudo != '' && req.body.email != '' && req.body.password != '' && emailregex.test(req.body.email) == true) {
 
     //hash password
     var hash = bcrypt.hashSync(req.body.password, 10);
@@ -46,8 +62,6 @@ if (emailregex.test(req.body.email) == false )
     var userSaved = await newUser.save();
     result = true
   }
-  console.log(result)
-  console.log(error)
   res.json(
     {
       error,
@@ -59,45 +73,61 @@ if (emailregex.test(req.body.email) == false )
 
 // Route sign-up more avec plus d'infos sur le user:
 
-router.put('/signup-more/:token', async (req, res) => {
-  console.log(req.body)
-  const { zipcode, city, livingPlace, guardType } = req.body
+router.post('/signup-more/:token', async (req, res) => {
+  var error = '';
+  var result = false;
 
-  if (zipcode == '' || city == '') {
+  var userData = JSON.parse(req.body.userInfo)
+
+  if (userData.zipcode == '' || userData.city == '') {
 
     error = 'Veuillez remplir tous les champs!'
-    result = false
 
   } else {
 
-    const userExist = await userModel.find({ token: req.params.token });
+    var userExist = await userModel.findOne({ token: req.params.token });
     //check if user exist
     if (!userExist) {
-      res.status(401)
-      throw new Error('User not found')
+      error = 'Utilisateur pas trouvé';
+
     } else {
-      result = true
-      const updateUser = await userModel.updateOne({ token: req.params.token },
-        {
-          livingPlace,
-          guardType,
-          $push: {
-            address: {
-              zipcode,
-              city
-            }
-          }
+      userExist.livingPlace = userData.livingPlace
+      userExist.guardType = userData.guardType
+      userExist.address.push({ zipcode: userData.zipcode, city: userData.city })
+
+      // Si le user n'existe pas on va envoyer l'image dans un fichier temporaire
+      var imagePath = './tmp/' + uniqid() + '.jpg';
+
+      var resultCopy = await req.files.image.mv(imagePath);
+
+      // Puis envoyer l'image sur Cloudinary et récupérer l'url dans la bdd
+      if (!resultCopy) {
+        var resultCloudinary = await cloudinary.uploader.upload(imagePath, {folder: 'petfriends'});
+        userExist.avatar = resultCloudinary.url
+      // On sauvegarde ensuite le user mis à jour avec les dernières infos  
+        var userUpdated = await userExist.save()
+
+        if (userUpdated) {
+          result = true
+
+        } else {
+          error = "user pas mis à jour"
         }
-      )
+      }
+      else {
+        error = "fichier non transféré"
+      }
     }
+
+    res.json({ result, error })
+   
+    fs.unlinkSync(imagePath)
 
   }
 
-  res.json({
-    error,
-    result
-  })
 })
+
+
 
 // Route Signin user :
 router.post('/signin', async (req, res) => {
